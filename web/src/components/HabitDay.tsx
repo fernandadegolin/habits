fd93e4951ecf4c5423f9840d0e4d83117fd1825d
fernanda.degolin <fernanda.degolin@g.globo>
2023-01-20 23:51:34 -0300

    feat(web): cria a tabela visual de dias de habitos

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+// type HabitsProps = {
+//   completed: number;
+// };
+
+export function HabitDay() {
+  return (
+    <div className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  );
+}
